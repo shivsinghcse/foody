e440a3f3a82925c0a3549aeb7dfcc73afc6743e6
Shiv Singh <shivsinghcse@gmail.com>
2025-02-09 23:19:20 +0530

    adding header

--- App.js
+++ App.js
@@ -1,13 +1,25 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import ReactDOM from 'react-dom/client';
 import Header from './src/Components/Header';
 // import './index.css';
 
 const App = () => {
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+    const fetchData = async () => {
+        const data = await fetch(
+            'https://thingproxy.freeboard.io/fetch/https://www.swiggy.com/dapi/restaurants/list/v5?lat=26.8466937&lng=80.94616599999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING'
+        );
+
+        const json = await data.json();
+
+        console.log(json);
+    };
     return (
         <>
             <Header />
-            <h1>This is App</h1>
         </>
     );
 };
--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -1,11 +1,38 @@
-import './Header.css';
+// import { Link } from 'react-router';
+import { LOGO_URL } from '../../../utils/constants';
+
 
 const Header = () => {
     return (
         <>
-            <div className="header">
-                <h1>This is Header</h1>
-            </div>
+            <header
+                style={{
+                    display: 'flex',
+                    justifyContent: 'space-between',
+                    alignItems: 'center',
+                    padding: '0.7rem 2rem',
+                }}
+            >
+                <div className="logo">
+                    {/* <Link to="/"> */}
+                    <img
+                        style={{
+                            width: '4rem',
+                            borderRadius: '50%',
+                            border: '1.5px solid #000',
+                        }}
+                        className="logo-img"
+                        src={LOGO_URL}
+                        alt="logo-image"
+                    />
+                    {/* </Link> */}
+                </div>
+
+                <nav>
+                    <button className="sign-in-btn">Sing In</button>
+                    <button className="cart-btn">Cart</button>
+                </nav>
+            </header>
         </>
     );
 };
