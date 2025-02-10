157b2c7e06ec632fbb35c28ffdc5783f06ef53db
Shiv Singh <shivsinghcse@gmail.com>
2025-02-10 17:36:32 +0530

    cors issue fixed

--- App.js
+++ App.js
@@ -14,7 +14,7 @@ const App = () => {
 
     const fetchData = async () => {
         const data = await fetch(
-            'https://thingproxy.freeboard.io/fetch/https://www.swiggy.com/dapi/restaurants/list/v5?lat=26.8466937&lng=80.94616599999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING'
+            'https://food-app-cors.vercel.app/api/proxy/swiggy/dapi/restaurants/list/v5?lat=26.8466937&lng=80.94616599999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING'
         );
 
         const json = await data.json();
