308f92a4150e74204e7476a1ad0542dd14bd05a7
Shiv Singh <shivsinghcse@gmail.com>
2025-02-10 18:26:24 +0530

    restarting

---
+++ src/App.js
@@ -0,0 +1,9 @@
+const App = () => {
+    return (
+        <>
+            <h1 className="">The food app</h1>
+        </>
+    );
+};
+
+export default App;
--- utils/constants.js
+++ utils/constants.js
@@ -3,3 +3,8 @@ export const LOGO_URL =
 
 export const CDN_URL =
     'https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_366/';
+
+
+
+    //  data = await fetch(
+    //         'https://food-app-cors.vercel.app/api/proxy/swiggy/dapi/restaurants/list/v5?lat=26.8466937&lng=80.94616599999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING'
