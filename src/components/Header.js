557a0a5edfb6fa1ecd704eb9ca9a2c30660f8f29
Shiv Singh <shivsinghcse@gmail.com>
2025-02-14 05:05:32 +0530

    making location drawer responsive

--- src/components/Header.js
+++ src/components/Header.js
@@ -36,7 +36,7 @@ const Header = () => {
                 onClick={hideSideMenu}
             >
                 <div
-                    className="bg-white w-[35vw] h-full absolute duration-[400ms]"
+                    className="bg-white w-full md:w-[50vw] lg:w-[35vw] h-full absolute duration-[400ms]"
                     style={{
                         left: toggle ? '0%' : '-100%',
                     }}
@@ -45,10 +45,10 @@ const Header = () => {
                     }}
                 >
                     <RxCross2
-                        className="font-extrabold absolute top-17 left-17 text-[24px] cursor-pointer"
+                        className="font-extrabold absolute top-[5%] left-[10%] text-[24px] cursor-pointer border-1 rounded-full"
                         onClick={hideSideMenu}
                     />
-                    <div className=" w-[70%] mx-auto absolute top-[15%] left-[15%]">
+                    <div className=" w-[70%]  mx-auto absolute top-[15%] left-[15%]">
                         <input
                             type="text"
                             placeholder="Search for area, street name.."
@@ -57,6 +57,7 @@ const Header = () => {
                     </div>
                 </div>
             </div>
+            {/* header  */}
             <header className="p-3 shadow-xl bg-white sticky top-0 left-0 z-50">
                 <div className="w-full md:w-[85%] xl:w-[70%] mx-auto  flex items-center overflow-hidden">
                     <Link to="/">
